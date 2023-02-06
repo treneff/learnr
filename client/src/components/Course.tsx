b19fd47fe0d3944343707589de4c25cc66eac67d
jamiemcglone <jamie.mcglone21@btinternet.com>
2023-02-06 11:08:26 +0000

    Added a welcome message

--- client/src/components/Course.tsx
+++ client/src/components/Course.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Course = () => {
-    return <div>Course</div>;
+    return <div>Course
+            Welcome to learnr!
+    </div>;
 };
 
 export default Course;
