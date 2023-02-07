75897a796429b75a3b5032a6857d017ffbb862c8
George Treneff <treneff@pm.me>
2023-02-07 21:04:16 +0000

    :package: added styled components and styled components types

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -2,6 +2,12 @@ import SideMenu from '../SideMenu';
 import styled from 'styled-components';
 
 const Header = () => {
+
+    const HeaderMain = styled.header`
+    
+    `
+
+    
     //     contains account menu on the right
     //     logo in the middle
     //     and sidemenu button opener on the left side
