7fcfdba0bae126fa47b5ae36244b4764ce438d79
EJDC <ewanconnor@gmail.com>
2023-02-07 20:55:57 +0000

    :construction: Dataloader - Days added.

--- server/src/main/java/com/learnr/Learnr/components/DataLoader.java
+++ server/src/main/java/com/learnr/Learnr/components/DataLoader.java
@@ -38,43 +38,66 @@ public class DataLoader implements ApplicationRunner {
 
     public void run(ApplicationArguments args) {
         //Create Course
-        Course softwareDev = new Course("Professional software development");
+        Course softwareDev = new Course("Professional Software Development");
         courseRepository.save(softwareDev);
 
-        //Days
-        Day jsFunctions = new Day("JavaScript Functions", 4, 4, softwareDev);
-        dayRepository.save(jsFunctions);
-        Day jsLoops = new Day("JavaScript Loops", 4, 3, softwareDev);
-        dayRepository.save(jsLoops);
-        Day pythonFunctions = new Day("Python Functions", 1, 3, softwareDev);
-        dayRepository.save(pythonFunctions);
-        Day pythonLoops = new Day("Python Loops", 1, 4, softwareDev);
-        dayRepository.save(pythonLoops);
-        Day javaClasses = new Day("Java Classes", 4, 4, softwareDev);
-        dayRepository.save(javaClasses);
+        //DAYS
+        //MODULE 1 (Weeks 1-4, 20 Days.  Week 1 Populated with actual titles)
+        Day W1D1 = new Day("Induction, Unix & Git", 1, 1, softwareDev);
+        dayRepository.save(W1D1);
+        Day W1D2 = new Day("Intro to Programming, Conditionals, Data Structures & Loops", 1, 2, softwareDev);
+        dayRepository.save(W1D2);
+        Day W1D3 = new Day("Functions, Tests & Pair Programming", 1, 3, softwareDev);
+        dayRepository.save(W1D3);
+        Day W1D4 = new Day("Friends Lab & Modules/Packages", 1, 4, softwareDev);
+        dayRepository.save(W1D4);
+        Day W1D5 = new Day("Weekend Homework & Recap", 1, 5, softwareDev);
+        dayRepository.save(W1D5);
+
+        Day W2D1 = new Day("Seize End-to-End Applications", 2, 1, softwareDev);
+        dayRepository.save(W2D1);
+        Day W2D2 = new Day("Integrate Granular E-commerce", 2, 2, softwareDev);
+        dayRepository.save(W2D2);
+        Day W2D3 = new Day("Leverage Web-Enabled Portals", 2, 3, softwareDev);
+        dayRepository.save(W2D3);
+        Day W2D4 = new Day("Integrate Back-end Networks", 2, 4, softwareDev);
+        dayRepository.save(W2D4);
+        Day W2D5 = new Day("Weekend Homework & Recap", 2, 5, softwareDev);
+        dayRepository.save(W2D5);
+
+        Day W3D1 = new Day("Enable Strategic Solutions", 3, 1, softwareDev);
+        dayRepository.save(W3D1);
+        Day W3D2 = new Day("Embrace 24/7 E-Markets", 3, 2, softwareDev);
+        dayRepository.save(W3D2);
+        Day W3D3 = new Day("Architect Front-end Channels", 3, 3, softwareDev);
+        dayRepository.save(W3D3);
+        Day W3D4 = new Day("Target Proactive Action-Items", 3, 4, softwareDev);
+        dayRepository.save(W3D4);
+        Day W3D5 = new Day("Weekend Homework & Recap", 3, 5, softwareDev);
+        dayRepository.save(W3D5);
 
         //Course Notes
-        CourseNote command_line_basics = new CourseNote("Command Line Basics", "This will be the content of Command Line Basics", jsFunctions);
+        CourseNote command_line_basics = new CourseNote("Command Line Basics", "This will be the content of Command Line Basics", W1D1);
         courseNoteRepository.save(command_line_basics);
-        CourseNote effective_text_editing = new CourseNote("Effective Text Editing", "This will be the content of Command Line Basics", jsFunctions);
+        CourseNote effective_text_editing = new CourseNote("Effective Text Editing", "This will be the content of Command Line Basics", W1D1);
         courseNoteRepository.save(effective_text_editing);
-        CourseNote git_and_github = new CourseNote("Git and Github", "This will be the content of Git and Github", jsLoops);
+        CourseNote git_and_github = new CourseNote("Git and Github", "This will be the content of Git and Github", W1D1);
         courseNoteRepository.save(git_and_github);
-        CourseNote what_is_programming = new CourseNote("What is programming", "This will be the content of what is programming", pythonFunctions);
+        CourseNote what_is_programming = new CourseNote("What is programming", "This will be the content of what is programming", W1D1);
         courseNoteRepository.save(what_is_programming);
 
         //Homework
-        Homework submit_to_github = new Homework("Submit Something to Github", "This will be the content of the Github Homework", javaClasses);
+        Homework submit_to_github = new Homework("Submit Something to Github", "This will be the content of the Github Homework", W1D1);
         homeworkRepository.save(submit_to_github);
-        Homework lists_and_dictionaries = new Homework("Lists and Dictionaries Homework", "This will be the content of the Github Homework", javaClasses);
+        Homework lists_and_dictionaries = new Homework("Lists and Dictionaries Homework", "This will be the content of the Github Homework", W1D1);
         homeworkRepository.save(lists_and_dictionaries);
 
         //Lab
-        Lab jsLab = new Lab("JavaScript Functions Lab", "Extensive Js Lab", jsFunctions);
+        Lab jsLab = new Lab("JavaScript Functions Lab", "Extensive Js Lab", W1D1);
         labRepository.save(jsLab);
-        Lab pythonLab = new Lab("Python Lab", "Extensive Python Lab", pythonFunctions);
+        Lab pythonLab = new Lab("Python Lab", "Extensive Python Lab", W1D1);
         labRepository.save(pythonLab);
-        Lab javaLab = new Lab("Java Lab", "Extensive Java Lab", jsLoops);
+        Lab javaLab = new Lab("Java Lab", "Extensive Java Lab", W1D1);
         labRepository.save(javaLab);
 
         //Teachers and Students
