a4d3e837ab6b6ab932838b22d87807a4fda97e4a
jamiemcglone <jamie.mcglone21@btinternet.com>
2023-02-07 14:23:03 +0000

    :construction: Controllers complete, testing in progress. References need reworked

---
+++ server/src/main/java/com/learnr/Learnr/controllers/CompletionController.java
@@ -0,0 +1,32 @@
+package com.learnr.Learnr.controllers;
+
+import com.learnr.Learnr.models.Completion;
+import com.learnr.Learnr.repositories.CompletionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CompletionController {
+    @Autowired
+    CompletionRepository completionRepository;
+
+    @GetMapping(value = "/completions")
+    public ResponseEntity<List<Completion>> getAllCompletions(){
+        return new ResponseEntity<>(completionRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/completions/{id}")
+    public ResponseEntity getCompletionById(@PathVariable Long id){
+        return new ResponseEntity<>(completionRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/completions")
+    public ResponseEntity<Completion> postCompletion(@RequestBody Completion completion){
+        completionRepository.save(completion);
+        return new ResponseEntity<>(completion, HttpStatus.CREATED);
+    }
+}
