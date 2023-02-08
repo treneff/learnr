bdbfde92b6ed006ac8a9891886117090650c8762
jamiemcglone <jamie.mcglone21@btinternet.com>
2023-02-08 14:37:26 +0000

    :construction: fixed the burger navigation to use links not anchors, restructed the header file structure

---
+++ client/src/components/header/SideMenu/Burger.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import styled from "styled-components";
+import colors from "../../../Global";
+
+type Props = {
+    open: boolean;
+    setOpen: (v: boolean) => void;
+};
+
+export const Burger = (props: Props) => (
+    <StyledBurger
+    open={props.open}
+    onClick={() => props.setOpen(!props.open)}
+    >
+        <div />
+        <div />
+        <div />
+    </StyledBurger>
+);
+
+const StyledBurger = styled.button<{open : boolean}>`
+    position: fixed;
+    left: 3vw;
+    top: 3vw;
+    width: 2rem;
+    height: 2rem;
+    padding: 0;
+    background: transparent;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    border: none;
+    cursor: pointer;
+    outline: none;
+    z-index: 1;
+    left: ${({open}) => (open ? "17vw" : "3vw")};
+
+    div {
+        position: relative;
+        width: 2rem;
+        height: 0.25rem;
+        border-radius: 10px;
+        background-color: ${({ open }) => 
+            open ? colors.get("backgroundColor") : colors.get("tertiaryColor")};
+
+        transition: all 0.3s linear;
+        transform-origin: 1px;
+
+        :first-child {
+            transform: ${({open}) =>
+            (open ? "rotate(45deg)" : "rotate(0)")};
+        }
+        :nth-child(2) {
+            opacity:${({open}) => (open ? "0" : "1")};
+            transform: ${({open}) =>
+                (open ? "translateX(20px)": "translateX(0)")};
+        }
+        :nth-child(3) {
+            transform: ${({open}) => 
+                (open ? "rotate(-45deg)": "rotate(0)")};
+        }
+    }
+`
---
+++ client/src/components/header/SideMenu/SideMenu.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Burger } from './Burger';
+import styled from 'styled-components';
+
+const SideMenu = () => {
+
+    const [open, setOpen] = useState<boolean>(false);
+    const close = () => setOpen(false);
+
+    return (
+        <>
+                <StyledSideMenu open={open}>
+                    <Link to="/" onClick={() => close()}><StyledSideLink>Home</StyledSideLink></Link>
+                    <Link to='/profile' onClick={() => close()}><StyledSideLink>Profile</StyledSideLink></Link>
+                    <Link to='/course' onClick={() => close()}><StyledSideLink>Course</StyledSideLink></Link>
+                    <Link to='/cohort' onClick={() => close()}><StyledSideLink>Cohort</StyledSideLink></Link>
+                    <Link to='/registration' onClick={() => close()}><StyledSideLink>Registration</StyledSideLink></Link>
+                    <Link to='/testing' onClick={() => close()}><StyledSideLink>Firestore Testing</StyledSideLink></Link>
+                </StyledSideMenu>
+                <Burger open={open} setOpen = {setOpen} />
+        </>
+    );
+};
+
+export default SideMenu;
+
+const StyledSideMenu = styled.nav<{open: boolean}>`
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 20vw;
+    position: fixed;
+    background-color: var(--tertiary-color);
+    z-index: 1;
+    padding: 10rem 0;
+    flex-direction: column;
+    display: ${({open}) => (open ? "flex" : "none")};
+    transition: transform 0.3s ease-in-out;
+    transform: ${({open}) => (
+        open ? "translateX(0)" : "translateX(-100%)")};
+`;
+
+const StyledSideLink = styled.span`
+    padding: 0 2rem;
+    font-size: 2rem;
+    color: var(--background-color);
+    text-decoration: none;
+
+    :hover {
+        color: var(--secondary-color);
+        cursor: pointer;
+    }
+`;
