6cfebc2fab5effbbc940e113d1403da604b501d6
EJDC <ewanconnor@gmail.com>
2023-02-08 16:38:28 +0000

    Registration page started - register with email and google

--- client/src/components/Registration.tsx
+++ client/src/components/Registration.tsx
@@ -1,7 +1,186 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../config/firebase";
+import {
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+} from "firebase/auth";
+// import { useAuthValue } from 'firebase/context';
+import styled from "styled-components";
+import colors from "../Global";
 
-const Registration = () => {
-    return <div>Registration</div>;
+const Registration: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmationPassword, setConfirmationPassword] = useState("");
+  const [error, setRegistrationError] = useState("");
+  const navigate = useNavigate();
+  //   const {setTimeActive} = useAuthValue()
+
+  const checkPassword = () => {
+    let passwordValid = true;
+    if (password !== null && confirmationPassword !== null) {
+      if (password !== confirmationPassword) {
+        passwordValid = false;
+        setRegistrationError("Passwords do not match! Try again!");
+      }
+    }
+    return passwordValid;
+  };
+
+  const register = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setRegistrationError("");
+    if (checkPassword() == true) {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then(() => {
+          if (auth.currentUser) {
+            sendEmailVerification(auth.currentUser)
+              .then(() => {
+                // setTimeActive(true);
+                navigate("/verify-email");
+              })
+              .catch((err: Error) => alert(err.message));
+          } else {
+            setRegistrationError("Error: User is not defined");
+          }
+        })
+        .catch((err: Error) => setRegistrationError(err.message));
+    }
+    setEmail("");
+    setPassword("");
+    setConfirmationPassword("");
+  };
+
+  const registerWithGoogle = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setRegistrationError("");
+    if (checkPassword() == true) {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then(() => {
+          if (auth.currentUser) {
+            sendEmailVerification(auth.currentUser)
+              .then(() => {
+                // setTimeActive(true);
+                navigate("/verify-email");
+              })
+              .catch((err: Error) => alert(err.message));
+          } else {
+            setRegistrationError("Error: User is not defined");
+          }
+        })
+        .catch((err: Error) => setRegistrationError(err.message));
+    }
+    setEmail("");
+    setPassword("");
+    setConfirmationPassword("");
+  };
+
+  const RegistrationContainer = styled.div`
+    position: relative;
+    z-index: 1;
+    max-width: 300px;
+    margin: 0 auto;
+  `;
+
+  const RegistrationForm = styled.form`
+    position: relative;
+    z-index: 1;
+    background: #ffffff;
+    max-width: 300px;
+    margin: 0 auto 100px;
+    padding: 30px;
+    border-top-left-radius: 3px;
+    border-top-right-radius: 3px;
+    border-bottom-left-radius: 3px;
+    border-bottom-right-radius: 3px;
+    text-align: center;
+  `;
+
+  const RegistrationFormInput = styled.input`
+    outline: 0;
+    background: #f2f2f2;
+    width: 100%;
+    border: 0;
+    margin: 0 0 15px;
+    padding: 15px;
+    border-top-left-radius: 3px;
+    border-top-right-radius: 3px;
+    border-bottom-left-radius: 3px;
+    border-bottom-right-radius: 3px;
+    box-sizing: border-box;
+    font-size: 14px;
+  `;
+
+  const RegistrationFormButton = styled.button`
+    outline: 0;
+    background: #ef3b3a;
+    width: 100%;
+    border: 0;
+    padding: 15px;
+    border-top-left-radius: 3px;
+    border-top-right-radius: 3px;
+    border-bottom-left-radius: 3px;
+    border-bottom-right-radius: 3px;
+    color: #ffffff;
+    font-size: 14px;
+    transition: all 0.3 ease;
+    cursor: pointer;
+  `;
+
+  
+
+
+
+
+  return (
+    <div className="center">
+      <RegistrationContainer>
+        <h1>Register</h1>
+        <RegistrationForm onSubmit={register} name="registration_form">
+        <RegistrationFormButton type="submit">
+            Register with Google
+          </RegistrationFormButton>
+          <p>or</p>
+          <RegistrationFormInput
+            type="email"
+            value={email}
+            placeholder="Enter your email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <RegistrationFormInput
+            type="password"
+            value={password}
+            required
+            placeholder="Enter your password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <RegistrationFormInput
+            type="password"
+            value={confirmationPassword}
+            required
+            placeholder="Confirm password"
+            onChange={(e) => setConfirmationPassword(e.target.value)}
+          />
+
+          <RegistrationFormButton type="submit">
+            Register
+          </RegistrationFormButton>
+  
+        </RegistrationForm>
+
+        {error && <div className="auth__error">{error}</div>}
+
+        <span>
+          Already have an account?
+          <Link to="/login">login</Link>
+        </span>
+      </RegistrationContainer>
+    </div>
+  );
 };
 
 export default Registration;
