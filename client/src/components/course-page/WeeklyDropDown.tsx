d88903f7a2c4bd1c9f3f82689713679277b5a0da
George Treneff <treneff@pm.me>
2023-02-11 14:37:07 +0000

    temporary changes to course

--- client/src/components/course-page/WeeklyDropDown.tsx
+++ client/src/components/course-page/WeeklyDropDown.tsx
@@ -2,31 +2,31 @@ import styled from 'styled-components';
 
 import WeekListItem from './WeekListItem';
 interface CourseProps {
-    course: any|number;
-    setOpenWeekNumber: React.Dispatch<React.SetStateAction<number>>
-    setOpenDayNumber: React.Dispatch<React.SetStateAction<number>>
+    course: any | number;
+    setOpenWeekNumber: React.Dispatch<React.SetStateAction<number>>;
+    setOpenDayNumber: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const WeeklyDropDown: React.FC<CourseProps> = ({course,setOpenWeekNumber,setOpenDayNumber}) => {
-    
+const WeeklyDropDown: React.FC<CourseProps> = ({ course, setOpenWeekNumber, setOpenDayNumber }) => {
+    let currentWeek = 0;
+
+    const weeklyNodes = course.course[0].days.map((weeklyContent: any, index: number) => {
+        if (weeklyContent.weekNumber !== currentWeek) {
+            currentWeek = weeklyContent.weekNumber;
+            return (
+                <WeekListItem
+                    key={index}
+                    weekNumber={weeklyContent.weekNumber}
+                    course={course}
+                    setOpenWeekNumber={setOpenWeekNumber}
+                    setOpenDayNumber={setOpenDayNumber}
+                />
+            );
+        } else return null;
+    });
     return (
         <WeeklyList>
-            <WeekListItem weekNumber={1} course={course} setOpenWeekNumber = {setOpenWeekNumber} setOpenDayNumber = {setOpenDayNumber} />
-            <WeekListItem weekNumber={2} course={course} setOpenWeekNumber = {setOpenWeekNumber} setOpenDayNumber = {setOpenDayNumber}/>
-            <WeekListItem weekNumber={3} course={course} setOpenWeekNumber = {setOpenWeekNumber} setOpenDayNumber = {setOpenDayNumber}/>
-            <WeekListItem weekNumber={4} course={course} setOpenWeekNumber = {setOpenWeekNumber} setOpenDayNumber = {setOpenDayNumber}/>
-            <WeekListItem weekNumber={5} course={course} setOpenWeekNumber = {setOpenWeekNumber} setOpenDayNumber = {setOpenDayNumber}/>
-            <WeekListItem weekNumber={6} course={course} setOpenWeekNumber = {setOpenWeekNumber} setOpenDayNumber = {setOpenDayNumber}/>
-            <WeekListItem weekNumber={7} course={course} setOpenWeekNumber = {setOpenWeekNumber} setOpenDayNumber = {setOpenDayNumber}/>
-            <WeekListItem weekNumber={8} course={course} setOpenWeekNumber = {setOpenWeekNumber} setOpenDayNumber = {setOpenDayNumber}/>
-            <WeekListItem weekNumber={9} course={course} setOpenWeekNumber = {setOpenWeekNumber} setOpenDayNumber = {setOpenDayNumber}/>
-            <WeekListItem weekNumber={10} course={course} setOpenWeekNumber = {setOpenWeekNumber} setOpenDayNumber = {setOpenDayNumber}/>
-            <WeekListItem weekNumber={11} course={course} setOpenWeekNumber = {setOpenWeekNumber} setOpenDayNumber = {setOpenDayNumber}/>
-            <WeekListItem weekNumber={12} course={course} setOpenWeekNumber = {setOpenWeekNumber} setOpenDayNumber = {setOpenDayNumber}/>
-            <WeekListItem weekNumber={13} course={course} setOpenWeekNumber = {setOpenWeekNumber} setOpenDayNumber = {setOpenDayNumber}/>
-            <WeekListItem weekNumber={14} course={course} setOpenWeekNumber = {setOpenWeekNumber} setOpenDayNumber = {setOpenDayNumber}/>
-            <WeekListItem weekNumber={15} course={course} setOpenWeekNumber = {setOpenWeekNumber} setOpenDayNumber = {setOpenDayNumber}/>
-            <WeekListItem weekNumber={16} course={course} setOpenWeekNumber = {setOpenWeekNumber} setOpenDayNumber = {setOpenDayNumber}/>
+            {course?{weeklyNodes}:null}
         </WeeklyList>
     );
 };
@@ -35,7 +35,7 @@ export default WeeklyDropDown;
 
 const WeeklyList = styled.ul`
     width: 40vw;
-    background-color:#F5F3F3;
-    padding:2rem;
-    overflow:auto;
+    background-color: #f5f3f3;
+    padding: 2rem;
+    overflow: auto;
 `;
