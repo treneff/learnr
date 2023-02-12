ff2e83f18a8c90d0d14eaa9a0bc5fbe726f1b612
jamiemcglone <jamie.mcglone21@btinternet.com>
2023-02-12 13:44:32 +0000

    :necktie: dashboard now displays each lesson for the day

--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Notes from "./Notes";
-import TodaysLessons from "./TodaysLessons";
+import Today from "./Today";
 import Tasks from "./Tasks";
 import styled from "styled-components";
 import DayService from "../../service/DayService";
@@ -21,21 +21,15 @@ const Dashboard: React.FC = () => {
         weekNumber: Number;
         dayNumber: Number;
         course: Object;
-        content: Array<ContentType>;
+        content: Array<any>;
     }
 
-    type ContentType = {
-        title: string;
-        detail: string;
-        day: DayType;
-    }
-    
     return(<>
         <MainTitle>Welcome, user</MainTitle>
         <DashboardItems>
             <DashboardItem>
             <h3>Todays Lessons</h3>
-            {dayToDisplay.map((day: DayType)=>{return <TodaysLessons title={day.title} content={day.content} weekNumber={day.weekNumber} dayNumber = {day.dayNumber} />})}
+            {dayToDisplay.map((day: DayType)=>{return <Today dayTitle={day.title} content={day.content} weekNumber={day.weekNumber} dayNumber = {day.dayNumber} />})}
             </DashboardItem>
             <DashboardItem>
                 <Tasks />
