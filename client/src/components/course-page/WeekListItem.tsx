478a299fe69e2dfe6e6670d5d1c01c8b6a113bc3
jamiemcglone <jamie.mcglone21@btinternet.com>
2023-02-15 16:46:01 +0000

    :sparkles: fixed week dropdown animation

--- client/src/components/course-page/WeekListItem.tsx
+++ client/src/components/course-page/WeekListItem.tsx
@@ -62,4 +62,5 @@ const ListItem = styled.li.attrs((props: { openWeekNumber: number; weekNumber: n
     height: ${(props) => (props.openWeekNumber === props.weekNumber ? 20 : 0)}%;
     transition: height 1s ease-in-out;
     max-height: content;
+    overflow: auto;
 `;
