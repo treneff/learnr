547f85bc24c3d3a72858ae50f4e919a5ebb1f5c8
George Treneff <treneff@pm.me>
2023-02-15 14:13:22 +0000

    :sparkles: mobile nav, mobile header, mobile cohort page and main

--- client/src/components/header/side-menu/SideMenu.tsx
+++ client/src/components/header/side-menu/SideMenu.tsx
@@ -1,51 +1,60 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import {Burger} from './Burger';
+import { Burger } from './Burger';
 import styled from 'styled-components';
 
 const SideMenu = () => {
-
     const [open, setOpen] = useState<boolean>(false);
     const close = () => setOpen(false);
 
     return (
         <>
-                <StyledSideMenu open={open}>
-                    <Link to="/" onClick={() => close()}><StyledSideLink>Home</StyledSideLink></Link>
-                    <Link to='/profile' onClick={() => close()}><StyledSideLink>Profile</StyledSideLink></Link>
-                    <Link to='/course' onClick={() => close()}><StyledSideLink>Course</StyledSideLink></Link>
-                    <Link to='/cohort' onClick={() => close()}><StyledSideLink>Cohort</StyledSideLink></Link>
-                    {/* <Link to='/registration' onClick={() => close()}><StyledSideLink>Registration</StyledSideLink></Link>
-                    <Link to='/testing' onClick={() => close()}><StyledSideLink>Firestore Testing</StyledSideLink></Link> */}
-                </StyledSideMenu>
-                <Burger open={open} setOpen = {setOpen} />
+            <StyledSideMenu open={open}>
+                <Link to='/' onClick={() => close()}>
+                    <StyledSideLink>Home</StyledSideLink>
+                </Link>
+                <Link to='/profile' onClick={() => close()}>
+                    <StyledSideLink>Profile</StyledSideLink>
+                </Link>
+                <Link to='/course' onClick={() => close()}>
+                    <StyledSideLink>Course</StyledSideLink>
+                </Link>
+                <Link to='/cohort' onClick={() => close()}>
+                    <StyledSideLink>Cohort</StyledSideLink>
+                </Link>
+            </StyledSideMenu>
+            <Burger open={open} setOpen={setOpen} />
         </>
     );
 };
 
 export default SideMenu;
 
-const StyledSideMenu = styled.nav<{open: boolean}>`
+const StyledSideMenu = styled.nav<{ open: boolean }>`
+    position: fixed;
+    z-index:9;
     top: 0;
     left: 0;
-    height: 100vh;
-    width: 20vw;
-    position: fixed;
-    background-color: var(--tertiary-color);
-    z-index: 1;
-    padding: 10rem 0;
+    display: flex;
     flex-direction: column;
-    display: ${({open}) => (open ? "flex" : "none")};
+    justify-content:center;
+    align-items:center;
+    height: 100vh;
+    min-width:content;
+    gap: 4rem;
+    background-color: var(--tertiary-color);
     transition: transform 0.3s ease-in-out;
-    transform: ${({open}) => (
-        open ? "translateX(0)" : "translateX(-100%)")};
+    transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(-100%)')};
+    @media (max-width:414px){
+        width:100%;
+    }
 `;
 
 const StyledSideLink = styled.span`
-    padding: 0 2rem;
-    font-size: 2rem;
+
+    font-size: 3rem;
     color: var(--background-color);
-    text-decoration: none;
+    padding: 0 4rem;
 
     :hover {
         color: var(--secondary-color);
